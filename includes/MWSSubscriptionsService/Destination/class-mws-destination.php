ff41dc34c050257f2a629743bc681bad6460045b
Jim <jmerk12086@gmail.com>
2020-08-13 12:32:13 -0400

    added method createRegisterDestinationRequest

--- includes/MWSSubscriptionsService/Destination/class-mws-destination.php
+++ includes/MWSSubscriptionsService/Destination/class-mws-destination.php
@@ -20,8 +20,17 @@ class MWS_Destination {
      */
     private static $serviceUrl = "https://mws.amazonservices.com/Subscriptions/2013-07-01";
 
+    function __construct() {
+
+
+
+
+
+
+    }
+
     /**
-     * HTTP Config for MWS Client
+     * HTTP Config for MWS Subscriptions Client
      * 
      * @since 0.6.0
      */
@@ -46,13 +55,54 @@ class MWS_Destination {
 
     }
 
+    /**
+     * Creates the request passed to method invokeRegisterDestination
+     * 
+     * @since 0.11.0
+     */
+    private static function createRegisterDestinationRequest() {
 
-    function __construct() {
+        $request = new MWSSubscriptionsService_Model_RegisterDestinationInput();
 
+        $request->setSellerId(MERCHANT_ID);
 
+        return $request;
+       
+    }
 
+    /**
+     * Get Register Destination Action Sample
+    * Gets competitive pricing and related information for a product identified by
+    * the MarketplaceId and ASIN.
+    *
+    * @param MWSSubscriptionsService_Interface $service instance of MWSSubscriptionsService_Interface
+    * @param mixed $request MWSSubscriptionsService_Model_RegisterDestination or array of parameters
+    */
+    private static function invokeRegisterDestination(MWSSubscriptionsService_Interface $service, $request)
+    {
 
+        try {
+            $response = $service->RegisterDestination($request);
 
+            echo ("Service Response\n");
+            echo ("=============================================================================\n");
+
+            $dom = new DOMDocument();
+            $dom->loadXML($response->toXML());
+            $dom->preserveWhiteSpace = false;
+            $dom->formatOutput = true;
+            echo $dom->saveXML();
+            echo("ResponseHeaderMetadata: " . $response->getResponseHeaderMetadata() . "\n");
+
+        } catch (MWSSubscriptionsService_Exception $ex) {
+            echo("Caught Exception: " . $ex->getMessage() . "\n");
+            echo("Response Status Code: " . $ex->getStatusCode() . "\n");
+            echo("Error Code: " . $ex->getErrorCode() . "\n");
+            echo("Error Type: " . $ex->getErrorType() . "\n");
+            echo("Request ID: " . $ex->getRequestId() . "\n");
+            echo("XML: " . $ex->getXML() . "\n");
+            echo("ResponseHeaderMetadata: " . $ex->getResponseHeaderMetadata() . "\n");
+        }
 
     }
 
