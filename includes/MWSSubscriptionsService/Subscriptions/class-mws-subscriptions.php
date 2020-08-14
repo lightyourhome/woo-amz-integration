cd10aaa382eb291243d2c199e6560cb35f65060f
Jim <jmerk12086@gmail.com>
2020-08-14 15:34:12 -0400

    added class MWS_Subscriptions and subscription model files

---
+++ includes/MWSSubscriptionsService/Subscriptions/class-mws-subscriptions.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+* Class responsible for Creating and Updating MWS Subscriptions
+* 
+* @version 0.1.0
+* @since 0.11.0
+*/
+
+class MWS_Subscriptions {
+
+    function __construct() {
+
+
+
+
+
+    }
+
+    
+
+
+
+
+}
