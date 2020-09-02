779cd67b460d66d7afbaf35533c2892fa1ea3602
Jim <jmerk12086@gmail.com>
2020-09-02 14:30:03 -0400

    added update subscription support

--- includes/MWSSubscriptionsService/Subscriptions/class-mws-subscriptions.php
+++ includes/MWSSubscriptionsService/Subscriptions/class-mws-subscriptions.php
@@ -12,6 +12,9 @@ require_once WOO_AMZ_PLUGIN_DIR . 'includes/MWSSubscriptionsService/Client.php';
 require_once WOO_AMZ_PLUGIN_DIR . 'includes/MWSSubscriptionsService/Model/CreateSubscriptionInput.php';
 require_once WOO_AMZ_PLUGIN_DIR . 'includes/MWSSubscriptionsService/Model/CreateSubscriptionResponse.php';
 require_once WOO_AMZ_PLUGIN_DIR . 'includes/MWSSubscriptionsService/Model/CreateSubscriptionResult.php';
+require_once WOO_AMZ_PLUGIN_DIR . 'includes/MWSSubscriptionsService/Model/UpdateSubscriptionInput.php';
+require_once WOO_AMZ_PLUGIN_DIR . 'includes/MWSSubscriptionsService/Model/UpdateSubscriptionResponse.php';
+require_once WOO_AMZ_PLUGIN_DIR . 'includes/MWSSubscriptionsService/Model/UpdateSubscriptionResult.php';
 require_once WOO_AMZ_PLUGIN_DIR . 'includes/MWSSubscriptionsService/Model/Subscription.php';
 require_once WOO_AMZ_PLUGIN_DIR . 'includes/MWSSubscriptionsService/Model/Destination.php';
 require_once WOO_AMZ_PLUGIN_DIR . 'includes/MWSSubscriptionsService/Model/AttributeKeyValueList.php';
@@ -33,6 +36,12 @@ class MWS_Subscriptions {
         if ( $action == 'CreateSubscription' ) {
 
             self::invokeCreateSubscription( self::getHttpClient(), self::createSubscriptionRequest() );
+
+        }
+
+        if ( $action == 'UpdateSubscription' ) {
+
+            self::invokeUpdateSubscription( self::getHttpClient(), self::updateSubscriptionRequest() );
 
         }
 
@@ -74,13 +83,11 @@ class MWS_Subscriptions {
      * 
      * @since 0.11.0
      * 
-     * @return MWSSubscriptionsService_Model_CreateSubscriptionInput $return
+     * @return MWSSubscriptionsService_Model_CreateSubscriptionInput $request
      */
     private static function createSubscriptionRequest() {
 
         $feed = new TFS_MWS_FEED();
-
-        $request = new MWSSubscriptionsService_Model_CreateSubscriptionInput();
 
         /**
          * Get an instance of MWSSubscriptionsService_Model_AttributeKeyValue and set the parameters
@@ -126,6 +133,8 @@ class MWS_Subscriptions {
     /**
      * Create an MWS Subscription and Log the Response
      * 
+     * @since 0.11.0
+     * 
      * @param MWSSubscriptionsService_Client $service
      * @param MWSSubscriptionsService_Model_CreateSubscriptionInput $request
      */
@@ -155,4 +164,95 @@ class MWS_Subscriptions {
        }
    }
 
-}
+    /**
+     * Create a subscription request to be used in invoking method invokeCreateSubscription
+     * 
+     * @since 0.11.0
+     * 
+     * @return MWSSubscriptionsService_Model_UpdateSubscriptionInput $request
+     */
+    private static function updateSubscriptionRequest() {
+
+        $feed = new TFS_MWS_FEED();
+
+        /**
+         * Get an instance of MWSSubscriptionsService_Model_AttributeKeyValue and set the parameters
+         */
+        $attributeKeyValue = new MWSSubscriptionsService_Model_AttributeKeyValue();
+        $attributeKeyValue->setKey('sqsQueueUrl');
+        $attributeKeyValue->setValue('https://sqs.us-east-2.amazonaws.com/838401002254/woo_amz_queue');
+
+        /**
+         * Get an instance of MWSSubscriptionsService_Model_AttributeKeyValueList and set the parameters
+         */
+        $attributeKeyValueList = new MWSSubscriptionsService_Model_AttributeKeyValueList();
+        $attributeKeyValueList->setmember( $attributeKeyValue );
+
+        /**
+         * Get an instance of MWSSubscriptionsService_Model_Destination, set the delivery channel and attribute list
+         */
+        $destination = new MWSSubscriptionsService_Model_Destination();
+        $destination->setDeliveryChannel( 'SQS' );
+        $destination->setAttributeList( $attributeKeyValueList );
+
+        /**
+         * Get an instance of MWSSubscriptionsService_Model_Subscription and set its parameters
+         */
+        $subscription = new MWSSubscriptionsService_Model_Subscription();
+        $subscription->setNotificationType('FeedProcessingFinished');
+        $subscription->setDestination( $destination );
+        $subscription->setIsEnabled(  TRUE );
+
+        /**
+         * Get an instance of MWSSubscriptionsService_Model_CreateSubscriptionInput and set its parameters
+         */
+        $request = new MWSSubscriptionsService_Model_UpdateSubscriptionInput();
+        $request->setSellerId( MERCHANT_ID );
+        $request->setMWSAuthToken( MERCHANT_ID );
+        $request->setMarketplaceId('ATVPDKIKX0DER');
+        $request->setSubscription( $subscription );
+        
+        return $request;
+
+    }
+
+
+   /**
+    * Update an existing Amazon Subscription and Log the response
+    *
+    * @since 0.11.0
+    * 
+    * @param MWSSubscriptionsService_Client $service
+    * @param MWSSubscriptionsService_Model_UpdateSubscriptionInput $request
+    */
+   private static function invokeUpdateSubscription($service, $request)
+   {
+       try {
+
+        $response = $service->UpdateSubscription($request);
+
+        echo ("Service Response\n");
+        echo ("=============================================================================\n");
+
+        $dom = new DOMDocument();
+        $dom->loadXML($response->toXML());
+        $dom->preserveWhiteSpace = false;
+        $dom->formatOutput = true;
+        echo $dom->saveXML();
+        echo("ResponseHeaderMetadata: " . $response->getResponseHeaderMetadata() . "\n");
+ 
+      } catch (MWSSubscriptionsService_Exception $ex) {
+
+        echo("Caught Exception: " . $ex->getMessage() . "\n");
+        echo("Response Status Code: " . $ex->getStatusCode() . "\n");
+        echo("Error Code: " . $ex->getErrorCode() . "\n");
+        echo("Error Type: " . $ex->getErrorType() . "\n");
+        echo("Request ID: " . $ex->getRequestId() . "\n");
+        echo("XML: " . $ex->getXML() . "\n");
+        echo("ResponseHeaderMetadata: " . $ex->getResponseHeaderMetadata() . "\n");
+
+      }
+
+    }
+
+} //END class MWS_Subscriptions
